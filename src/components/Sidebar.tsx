4b6af08ad307ae3b5cfa8e12785443060b293fbb
0xDrew <andrewakyampong@gmail.com>
2026-02-12 09:35:13 +0000

    Update sidebar header display name from 0xDrew to Akyampong

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -127,7 +127,7 @@ const SidebarHeader = () => {
         className="object-cover object-top rounded-full flex-shrink-0"
       />
       <div className="flex text-sm flex-col">
-        <p className="font-semibold text-neutral-900 dark:text-white text-base">0xDrew</p>
+        <p className="font-semibold text-neutral-900 dark:text-white text-base">Akyampong</p>
         <p className="text-neutral-600 dark:text-neutral-400 text-xs">Designer & Developer</p>
       </div>
     </div>
